9db95a0bd9aaa337fab1076a63ab37f5303a8bff
carrillocarlosce <bestguru.peru@gmail.com>
2018-09-17 11:10:29 -0400

    testing file upload - test 2

--- react/features/large-video/components/LargeVideo.web.js
+++ react/features/large-video/components/LargeVideo.web.js
@@ -605,7 +605,7 @@ export default class LargeVideo extends Component<*> {
 
                         <form ref='uploadForm'
                               id='uploadForm'
-                              action='http://localhost:8000/upload'
+                              action='http://localhost:8081/upload'
                               method='post'
                               encType="multipart/form-data">
                             <input type="file" name="sampleFile" />
